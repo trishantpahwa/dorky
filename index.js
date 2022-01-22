d4c583bf726311e456fdf361a81bba610d4c3e8e
Trishant Pahwa <trishantpahwa@gmail.com>
2022-01-22 10:10:07 +0530

    Seperated function to list files.

--- index.js
+++ index.js
@@ -3,27 +3,36 @@ const chalk = require('chalk');
 
 const exclusions = ['node_modules', 'tester'];
 
-var getDirectories = function (src, callback) {
-    glob(src + '/**/*', callback);
-};
+function listFiles() {
+    var getDirectories = function (src, callback) {
+        glob(src + '/**/*', callback);
+    };
 
-function excludeIsPresent(element) {
-    let present = false;
-    let i = 0;
-    while(i<exclusions.length) {
-        if(element.includes(exclusions[i])) present = true;
-        i += 1;
+    function excludeIsPresent(element) {
+        let present = false;
+        let i = 0;
+        while(i<exclusions.length) {
+            if(element.includes(exclusions[i])) present = true;
+            i += 1;
+        }
+        return present;
     }
-    return present;
+
+    getDirectories(__dirname, function (err, res) {
+        if (err) {
+            console.log('Error', err);
+        } else {
+            let listOfFiles;
+            listOfFiles = res.filter(element => !excludeIsPresent(element));
+            console.log(chalk.green('Found files:'))
+            listOfFiles.map((file) => console.log('\t' + chalk.bgGrey(file)));
+        }
+    });
 }
 
-getDirectories(__dirname, function (err, res) {
-    if (err) {
-        console.log('Error', err);
-    } else {
-        let listOfFiles;
-        listOfFiles = res.filter(element => !excludeIsPresent(element));
-        console.log(chalk.green('Found files:'))
-        listOfFiles.map((file) => console.log('\t' + chalk.bgGrey(file)));
+const args = process.argv.splice(2, 2);
+if(args.length == 1) {
+    if(args[0] == 'list') {
+        listFiles();
     }
-});
+}
