587fa4df6375c06bbc97acbc12cb4fa7d88d0eaf
Trishant Pahwa <trishantpahwa@gmail.com>
2022-01-22 09:57:16 +0530

    Excludes files and folders that are not required.

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const glob = require("glob");
 
+const exclusions = ['node_modules', 'tester'];
+
 var getDirectories = function (src, callback) {
-  glob(src + '/**/*', callback);
+    glob(src + '/**/*', callback);
 };
+
+function excludeIsPresent(element) {
+    let present = false;
+    let i = 0;
+    while(i<exclusions.length) {
+        if(element.includes(exclusions[i])) present = true;
+        i += 1;
+    }
+    return present;
+}
+
 getDirectories(__dirname, function (err, res) {
-  if (err) {
-    console.log('Error', err);
-  } else {
-    console.log(res);
-  }
+    if (err) {
+        console.log('Error', err);
+    } else {
+        let listOfFiles;
+        listOfFiles = res.filter(element => !excludeIsPresent(element));
+        console.log(listOfFiles);
+    }
 });
