09969662941305772350416f67ddac0a4b2ad1ef
Trishant Pahwa <trishantpahwa@gmail.com>
2024-10-13 14:34:03 +0530

    Added option for Google Drive.

--- bin/index.js
+++ bin/index.js
@@ -10,6 +10,14 @@ const mimeTypes = require("mime-types");
 const md5 = require('md5');
 const EOL = require("os").type() == "Darwin" ? "\r\n" : "\n";
 const { GetObjectCommand, PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
+
+// Google Drive config ************************************************************
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+const CREDENTIALS_PATH = path.join(__dirname, "../google-drive-credentials.json");
+const TOKEN_PATH = path.join(process.cwd(), '.dorky/credentials.json');
+// ********************************************************************************
 
 const figlet = `
       __            __          \t
@@ -33,7 +41,6 @@ if (process.argv.slice(2).length === 0) {
 }
 
 var args = yargs
-    .help('help')
     .option("init", { alias: "i", describe: "Initialize dorky project", type: "string", demandOption: false })
     .option("list", { alias: "l", describe: "List files in dorky", type: "string", demandOption: false })
     .option("add", { alias: "a", describe: "Add files to push or pull", type: "array", demandOption: false })
@@ -41,24 +48,52 @@ var args = yargs
     .option("push", { alias: "ph", describe: "Push files to storage", type: "string", demandOption: false })
     .option("pull", { alias: "pl", describe: "Pull files from storage", type: "string", demandOption: false })
     .option("migrate", { alias: "m", describe: "Migrate dorky project to another storage", type: "string", demandOption: false })
-    .argv;
+    .help('help')
+    .strict()
+    .argv
 
-function init(storage) {
-    function setupFilesAndFolders(metaData, credentials) {
-        console.log("Initializing dorky project");
-        if (existsSync(".dorky")) {
-            console.log("Dorky is already initialised in this project.");
-        } else {
-            mkdirSync(".dorky");
-            console.log(chalk.bgGreen("Created .dorky folder."));
-            writeFileSync(".dorky/metadata.json", JSON.stringify(metaData, null, 2));
-            console.log(chalk.bgGreen("Created .dorky/metadata.json file."));
-            writeFileSync(".dorkyignore", "");
-            console.log(chalk.bgGreen("Created .dorkyignore file."));
-            writeFileSync(".dorky/credentials.json", JSON.stringify(credentials, null, 2));
-            console.log(chalk.bgGreen("Created .dorky/credentials.json file."));
+if (Object.keys(args).length == 2) {
+    yargs.showHelp()
+}
+
+function setupFilesAndFolders(metaData, credentials) {
+    console.log("Initializing dorky project");
+    if (existsSync(".dorky")) {
+        console.log("Dorky is already initialised in this project.");
+    } else {
+        mkdirSync(".dorky");
+        console.log(chalk.bgGreen("Created .dorky folder."));
+        writeFileSync(".dorky/metadata.json", JSON.stringify(metaData, null, 2));
+        console.log(chalk.bgGreen("Created .dorky/metadata.json file."));
+        writeFileSync(".dorkyignore", "");
+        console.log(chalk.bgGreen("Created .dorkyignore file."));
+        writeFileSync(".dorky/credentials.json", JSON.stringify(credentials, null, 2));
+        console.log(chalk.bgGreen("Created .dorky/credentials.json file."));
+    }
+}
+
+async function authorizeGoogleDriveClient() {
+    async function loadSavedCredentialsIfExist() {
+        try {
+            const content = await fs.readFileSync(TOKEN_PATH);
+            const credentials = JSON.parse(content);
+            return google.auth.fromJSON(credentials);
+        } catch (err) {
+            return null;
         }
     }
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    return client;
+}
+
+async function init(storage) {
     const metaData = { "stage-1-files": {}, "uploaded-files": {} };
     var credentials;
     switch (storage) {
@@ -67,7 +102,8 @@ function init(storage) {
             setupFilesAndFolders(metaData, credentials);
             break;
         case "google-drive":
-            credentials = { storage: "google-drive" } // Setup credentials => TP | 2024-09-28 16:04:51
+            const client = await authorizeGoogleDriveClient();
+            credentials = { storage: "google-drive", ...client.credentials };
             setupFilesAndFolders(metaData, credentials);
             break;
         default:
@@ -98,6 +134,10 @@ function add(listOfFiles) {
     console.log("Adding files to stage-1 to push to storage");
     const metaData = JSON.parse(fs.readFileSync(".dorky/metadata.json"));
     listOfFiles.forEach((file) => {
+        if (!fs.existsSync(file)) {
+            console.log(chalk.red(`File ${file} does not exist.`));
+            return;
+        }
         const fileContents = fs.readFileSync(file);
         const fileType = mimeTypes.lookup(file);
         metaData["stage-1-files"][file] = {
@@ -158,16 +198,12 @@ function push() {
         }
     });
     const credentials = JSON.parse(fs.readFileSync(".dorky/credentials.json"));
-    if (filesToPush.length == 0) {
-        console.log(chalk.red("No files to push"));
-        return;
-    }
     switch (credentials.storage) {
         case "aws":
             pushToS3(filesToPush, credentials);
             break;
         case "google-drive":
-            pushToGoogleDrive(filesToPush, credentials);
+            pushToGoogleDrive(filesToPush);
             break;
         default:
             console.log("Please provide a valid storage option <aws|google-drive>");
@@ -188,19 +224,73 @@ function pushToS3(files, credentials) {
     });
     const bucketName = credentials.bucket ?? process.env.BUCKET_NAME;
     Promise.all(files.map(async (file) => {
+        const rootFolder = path.basename(process.cwd());
+        const pathToFile = path.join(rootFolder, file.name);
         await s3.send(
             new PutObjectCommand({
                 Bucket: bucketName,
-                Key: file.name,
+                Key: pathToFile,
                 Body: fs.readFileSync(file.name).toString(),
             })
         );
-        console.log(chalk.green(`Pushed ${file.name} to storage.`));
+        console.log(chalk.green(`Pushed ${pathToFile} to storage.`));
     }));
 }
 
-function pushToGoogleDrive(files, credentials) {
+
+async function pushToGoogleDrive(files) {
+    async function getOrCreateFolderId(folderPath, drive) {
+        const folders = folderPath.split(path.sep);
+        let parentId = 'root';
+        for (const folder of folders) {
+            const res = await drive.files.list({
+                q: `name='${folder}' and mimeType='application/vnd.google-apps.folder' and '${parentId}' in parents`,
+                fields: 'files(id, name)',
+                spaces: 'drive'
+            });
+            if (res.data.files.length > 0) {
+                parentId = res.data.files[0].id;
+            } else {
+                const folderMetadata = {
+                    name: folder,
+                    mimeType: 'application/vnd.google-apps.folder',
+                    parents: [parentId],
+                };
+                const folderRes = await drive.files.create({
+                    requestBody: folderMetadata,
+                    fields: 'id',
+                });
+                parentId = folderRes.data.id;
+            }
+        }
+        return parentId;
+    }
     console.log("Uploading to google drive");
+    const client = await authorizeGoogleDriveClient();
+    const drive = google.drive({ version: 'v3', auth: client });
+    for (const file of files) {
+        const rootFolder = path.basename(process.cwd());
+        const pathToFile = path.join(rootFolder, file.name);
+        const requestBody = {
+            name: path.basename(file.name),
+            parents: [await getOrCreateFolderId(pathToFile.split("/").slice(0, -1).join("/"), drive)],
+            fields: 'id',
+        };
+        const media = {
+            mimeType: file["mime-type"],
+            body: fs.createReadStream(path.join(process.cwd(), file.name)),
+        };
+        try {
+            await drive.files.create({
+                requestBody,
+                media: media,
+            });
+            console.log(chalk.green(`Pushed ${file.name} to storage.`));
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    }
 }
 
 function pull() {
@@ -217,7 +307,7 @@ function pull() {
             pullFromS3(filesToPull, credentials);
             break;
         case "google-drive":
-            pullFromGoogleDrive();
+            pullFromGoogleDrive(filesToPull);
             break;
         default:
             console.log("Please provide a valid storage option <aws|google-drive>");
@@ -235,19 +325,66 @@ function pullFromS3(files, credentials) {
     });
     const bucketName = credentials.bucket ?? process.env.BUCKET_NAME;
     Promise.all(Object.keys(files).map(async (file) => {
+        const rootFolder = path.basename(process.cwd());
+        const pathToFile = path.join(rootFolder, file);
         const { Body } = await s3.send(
             new GetObjectCommand({
                 Bucket: bucketName,
-                Key: file,
+                Key: pathToFile,
             })
         );
+        const dir = path.dirname(file);
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
         fs.writeFileSync(file, await Body.transformToString());
         console.log(chalk.green(`Pulled ${file} from storage.`));
     }));
 }
 
-function pullFromGoogleDrive(files, credentials) {
+async function pullFromGoogleDrive(files) {
     console.log("Downloading from google drive");
+    files = Object.keys(files).map((file) => {
+        return { name: file, ...files[file] };
+    });
+
+    const client = await authorizeGoogleDriveClient();
+    const drive = google.drive({ version: "v3", auth: client });
+    try {
+        files.map(async (file) => {
+            const res = await drive.files.list({
+                q: `name='${path.basename(file.name)}' and mimeType!='application/vnd.google-apps.folder'`,
+                fields: 'files(id, name)',
+                spaces: 'drive'
+            });
+            if (res.data.files.length === 0) {
+                console.log(chalk.red(`File ${file.name} not found in Google Drive.`));
+                return;
+            }
+            const _file = await drive.files.get({ fileId: res.data.files[0].id, alt: "media" });
+            const dir = path.dirname(file.name);
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, { recursive: true });
+            }
+            fs.writeFileSync(file.name, await _file.data.text(), "utf-8");
+            console.log(chalk.green(`Pulled ${file.name} from storage.`));
+            //         const res = await drive.files.list({
+            //             q: `name='${file.name}'`,
+            //             fields: 'files(id, name)',
+            //             spaces: 'drive'
+            //         });
+            //         const _file = await drive.files.get({ fileId: res.data.files[0].id, alt: "media" });
+            //         fs.writeFile(file.name, await _file.data.text(), "utf-8", (err) => {
+            //             if (err) {
+            //                 console.log(err);
+            //                 throw err;
+            //             }
+            //             console.log(chalk.green(`Pulled ${file.name} from storage.`));
+            //         });
+        });
+    } catch (err) {
+        throw err;
+    }
 }
 
 if (Object.keys(args).includes("init")) init(args.init);
@@ -255,4 +392,4 @@ if (Object.keys(args).includes("list")) list(args.list);
 if (Object.keys(args).includes("add")) add(args.add);
 if (Object.keys(args).includes("rm")) rm(args.rm);
 if (Object.keys(args).includes("push")) push();
-if (Object.keys(args).includes("pull")) pull();
+if (Object.keys(args).includes("pull")) pull(); 
